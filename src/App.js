d5f0a74fa523193e4ca39de13c2f87f20f2eda85
joshua-paragoso <paragoso90@gmail.com>
2020-10-30 09:46:48 -1000

    added a dropdown list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1>covid tracker</h1>
+      
+      {/*Drop down component */}
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+            <MenuItem value="worldWide">option1</MenuItem>
+            <MenuItem value="worldWide">option2</MenuItem>
+            <MenuItem value="worldWide">option3</MenuItem>
+            <MenuItem value="worldWide">option4</MenuItem>
+
+          </Select>
+      </FormControl>
 
       {/*Header */}
       {/* Title and search input dropdown field */}
