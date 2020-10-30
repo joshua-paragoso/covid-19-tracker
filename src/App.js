61620880df9487100852d83cdd5fac3ce27fa4c0
joshua-paragoso <paragoso90@gmail.com>
2020-10-30 08:46:45 -1000

    added setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="app">
       <h1>covid tracker</h1>
+      {/*Header */}
+      {/* Title and search input dropdown field */}
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+
+      {/* Map */}
+
     </div>
   );
 }
