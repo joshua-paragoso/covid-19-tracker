c45e3bf38c010b9ef674b6d202c553ebf57854db
joshua-paragoso <paragoso90@gmail.com>
2020-10-30 10:03:26 -1000

    countries appear in drop down list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  /* State = how to write a varibale in react */
+  const[countries, setCountries] = useState([
+    'usa', 'uk', 'india'
+  ]);
+
+  //https://disease.sh/v3/covid-19/countries
+
+  /*useEffect = runs a piece of code based on given condition*/
+  //runs once
+  useEffect(() => {
+
+  }, [countries]);
   return (
     <div className="app">
+      <div className="app__header">
+
+      
       <h1>covid tracker</h1>
       
       {/*Drop down component */}
       <FormControl className="app__dropdown">
         <Select
           variant="outlined"
-          value="abc"
-        >
-            <MenuItem value="worldWide">option1</MenuItem>
-            <MenuItem value="worldWide">option2</MenuItem>
-            <MenuItem value="worldWide">option3</MenuItem>
-            <MenuItem value="worldWide">option4</MenuItem>
+          value="abc">
+
+          {/*loop through all the countries as show a dropdown list of all the options*/}
+          {
+            countries.map(country => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))
+          }
 
           </Select>
       </FormControl>
+    </div>
 
       {/*Header */}
       {/* Title and search input dropdown field */}
