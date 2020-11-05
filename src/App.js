49731008986efc15a112227f2807b89e2d3eef22
joshua-paragoso <paragoso90@gmail.com>
2020-11-05 11:31:11 -1000

    tried to make a change for states instead of countries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import InfoBox from './InfoBox';
 import Map from "./Map";
 
-
 function App() {
   
   /* State = how to write a varibale in react */
@@ -131,4 +130,128 @@ function App() {
   );
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// function App() {
+  
+//   /* State = how to write a varibale in react */
+//   const [states, setStates] = useState([]);
+//   const [state, setState] = useState('unitedstates');
+//   const [stateInfo, setStateInfo] = useState({});
+
+//   //display worldwide stats as the initial stats
+//   useEffect(()=> {
+//     fetch("https://disease.sh/v3/covid-19/states")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       //set country info
+//       setStateInfo(data);
+//     });
+//   }, []);
+
+
+//   console.log('State INFO >>>', stateInfo);
+
+//   //https://disease.sh/v3/covid-19/countries
+
+//   /*useEffect = runs a piece of code based on given condition*/
+//   //runs once when code goes
+//   useEffect(() => {
+//     //async -> send a request, wait for it, do soemthing with it
+//     const getStatesData = async () => {
+//       await fetch("https://disease.sh/v3/covid-19/states")
+//       // await fetch("https://disease.sh/v3/covid-19/countries")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         //restructure
+//         const state = data.map((state) => (
+//           {
+//             name: state.state, //united states, united kingodm
+//           }
+//         ));
+//         setStates(state);
+//       });
+//     };
+//     getStatesData();
+//   }, []);
+  
+//   //changes value in dropdown list of what country is selected
+//   const onStateChange = async (event) => {
+//     const stateCode = event.target.value;
+//     // console.log("works >>>>", countryCode);
+//     setState(stateCode);
+
+//     //if country code is worldwide
+//     const url = stateCode === 'unitedstates' 
+//     ? `https://disease.sh/v3/covid-19/countries/states` 
+//     //else url is based on country code
+//     : `https://disease.sh/v3/covid-19/states/${stateCode}`;
+
+//     await fetch(url)
+//     //convert to json
+//     .then(response => response.json())
+//     //set country based on data
+//     .then(data => {
+//       //set country
+//       setState(stateCode);
+//       //set country info
+//       setStateInfo(data);
+//     });    
+//   };
+
+//   return (
+//     <div className="app">
+//       <div className="app__left">
+//         <div className="app__header">
+
+//         {/*Header */}
+//         <h1>covid tracker</h1>
+
+//        {/* Title and search input dropdown field */}
+//         <FormControl className="app__dropdown">
+//          <Select 
+//             variant="outlined" 
+//             onChange={onStateChange} value={state}>
+
+//             <MenuItem value="unitedstates">United States</MenuItem>
+//             {/*loop through all the countries as show a dropdown list of all the options*/}
+          
+//             {
+//               states.map(state => (
+//                 <MenuItem value={state.value}>{state.name}</MenuItem>
+//               ))
+//             }
+
+//           </Select>
+//         </FormControl>
+//     </div>
+    
+//       {/* InfoBoxes section */}
+//      <div className="app__stats">
+
+//         {/*InfoBoxes title = "Coronavirus cases"*/}
+//         <InfoBox title="Coronavirus cases" cases={stateInfo.todayCases} total={stateInfo.cases}/>
+      
+//         {/*InfoBoxes title = Coronavirus deaths*/}
+//         <InfoBox title="Deaths" cases={stateInfo.todayDeaths} total={stateInfo.deaths}/>
+      
+//       </div>
+    
+//         {/* Map */}
+//         <Map/>
+
+//       </div>
+
+//       <Card className="app__right">
+//         <CardContent>
+//           <h3>live cases by country</h3>
+//           <h3>worldwide new cases</h3>
+//           {/*Table*/}
+
+//           {/*Graph*/}
+//         </CardContent>
+//       </Card>
+//     </div>
+//   );
+// }
 export default App;
