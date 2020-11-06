49d185baa9259aae4faec21b0c7e5f2490b89180
joshua-paragoso <paragoso90@gmail.com>
2020-11-06 09:35:05 -1000

    installed numeral which can display the numbers as you hover over the graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,8 +1,53 @@
 import React, {useState, useEffect} from 'react';
 import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
-function LineGraph() {
-    
+const options = {
+    legend: {
+      display: false,
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+      mode: "index",
+      intersect: false,
+      callbacks: {
+        label: function (tooltipItem, data) {
+          return numeral(tooltipItem.value).format("+0,0");
+        },
+      },
+    },
+    scales: {
+        xAxes: [
+          {
+            type: "time",
+            time: {
+              format: "MM/DD/YY",
+              tooltipFormat: "ll",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              // Include a dollar sign in the ticks
+              callback: function (value, index, values) {
+                return numeral(value).format("0a");
+              },
+            },
+          },
+        ],
+      },
+    };
+
+function LineGraph() {   
     const [data, setData] = useState({})
 
     const buildChartData = (data, casesType="cases") => {
@@ -57,7 +102,9 @@ function LineGraph() {
                             data: data, 
                 },
                 ],
-            }}/>
+            }}
+            options={options}
+            />
         </div>
     );
 }
