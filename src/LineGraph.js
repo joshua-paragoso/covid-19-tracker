f34ef84cf6b66393d21cdc38716db6e6f2eab2b2
joshua-paragoso <paragoso90@gmail.com>
2020-11-06 09:51:11 -1000

    fixed apply error due to data not being populated

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,112 +1,117 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
 
 const options = {
-    legend: {
-      display: false,
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
     },
-    elements: {
-      point: {
-        radius: 0,
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
       },
     },
-    maintainAspectRatio: false,
-    tooltips: {
-      mode: "index",
-      intersect: false,
-      callbacks: {
-        label: function (tooltipItem, data) {
-          return numeral(tooltipItem.value).format("+0,0");
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
         },
       },
-    },
-    scales: {
-        xAxes: [
-          {
-            type: "time",
-            time: {
-              format: "MM/DD/YY",
-              tooltipFormat: "ll",
-            },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
           },
-        ],
-        yAxes: [
-          {
-            gridLines: {
-              display: false,
-            },
-            ticks: {
-              // Include a dollar sign in the ticks
-              callback: function (value, index, values) {
-                return numeral(value).format("0a");
-              },
-            },
-          },
-        ],
+        },
       },
-    };
+    ],
+  },
+};
 
-function LineGraph() {   
-    const [data, setData] = useState({})
+const buildChartData = (data, casesType = "cases") => {
+  let chartData = [];
+  let lastDataPoint;
 
-    const buildChartData = (data, casesType="cases") => {
-        
-        const chartData = [];
-        let lastDataPoint;
-
-        //loop through all data cases
-        //cases was recovered and deaths
-        // data[casesType].forEach(date => {
-            for (let date in data.cases) {
-            //if last data point
-            if(lastDataPoint){
-
-                //create new data point
-                const newDataPoint = {
-                    x: date,
-                    //access all cases
-                    //try to get the difference between dates to get how much new cases 
-                    //came up
-                    y: data[casesType][date] - lastDataPoint,
-                }
-                //push new data intp charData
-                chartData.push(newDataPoint);
-            } 
-                //reinitialize last data point
-                lastDataPoint = data[casesType][date];
-        } 
-            return chartData;
+  //loop through all data cases
+  //cases was recovered and deaths
+  // data[casesType].forEach(date => {
+  for (let date in data.cases) {
+    //if last data point
+    if (lastDataPoint) {
+      //create new data point
+      const newDataPoint = {
+        x: date,
+        //access all cases
+        //try to get the difference between dates to get how much new cases
+        //came up
+        y: data[casesType][date] - lastDataPoint,
+      };
+      //push new data intp charData
+      chartData.push(newDataPoint);
     }
-    //gets the last 120 days
-    //https://disease.sh/v3/covid-19/historical/all?lastdays=120
+    //reinitialize last data point
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
 
-    useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+function LineGraph({casesType}) {
+  const [data, setData] = useState({});
+
+  //gets the last 120 days
+  //https://disease.sh/v3/covid-19/historical/all?lastdays=120
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            const chartData = buildChartData(data);
-            setData(chartData);
+          console.log(data);
+          const chartData = buildChartData(data);
+          setData(chartData);
         });
-    }, []);
+    };
+    fetchData();
+  }, [casesType]);
 
-    return (
-        <div>
-            <h3> I am graph</h3>
-            <Line 
-                data ={{
-                    datasets: [{
-                            backgroundColor: "rgba(204, 16, 52, 0.5)",
-                            borderColor: "#CC1034",
-                            data: data, 
-                },
-                ],
-            }}
-            options={options}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h3> I am graph</h3>
+      {data?.length > 0 && (
+      <Line
+        data={{
+          datasets: [
+            {
+              backgroundColor: "rgba(204, 16, 52, 0.5)",
+              borderColor: "#CC1034",
+              data: data,
+            },
+          ],
+        }}
+        options={options}
+      />
+      )}
+    </div>
+  );
 }
 
-export default LineGraph
+export default LineGraph;
