998c8da81bfea587f489285172ffb1d04147563a
joshua-paragoso <paragoso90@gmail.com>
2020-11-12 15:12:16 -1000

    infobox now changes red when its either deaths or cases

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, active, total, ...props }) {
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
   return (
     <Card 
       onClick={props.onClick} 
-      className={`infoBox ${active && 'infoBox--selected'}`}>
+      className={`infoBox 
+      ${active && 'infoBox--selected'}
+      ${isRed && 'infoBox--red'}` 
+      }>
 
       <CardContent>
         {/*Title*/}
