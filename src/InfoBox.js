e2b516f3b6022aca5e4e16b3192a6437434e4a1d
joshua-paragoso <paragoso90@gmail.com>
2020-11-12 14:17:14 -1000

    line graph now changes based on caseType

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({title, cases, total, ...props}) {
-    return (
-        <Card 
-        onClick={props.onClick} 
-        className="infoBox">
-           
-            <CardContent>
-                {/*Title*/}
-                <Typography className="infoBox__title" color="textSecondary">
-                    {title}
-                </Typography>
+function InfoBox({ title, cases, total, ...props }) {
+  return (
+    <Card 
+      onClick={props.onClick} 
+      className="infoBox">
+        
+      <CardContent>
+        {/*Title*/}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
 
-                
+        {/* Number of cases */}
+        <h2 className="infoBox__cases">{cases}</h2>
 
-                {/* Number of cases */}
-                <h2 className="infoBox__cases">{cases}</h2>
-                
-                {/*Total */}
-                <Typography className="infoBox__total" color="textSecondary">
-                    {total} Total
-                </Typography>
-
-            </CardContent> 
-        </Card>
-    )
+        {/*Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
--- src/LineGraph.js
+++ src/LineGraph.js
@@ -91,9 +91,11 @@ function LineGraph({casesType}) {
     const fetchData = async () => {
       await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
         .then((response) => response.json())
+        // .then((response) => { return response.json(); })
         .then((data) => {
           console.log(data);
-          const chartData = buildChartData(data);
+          // const chartData = buildChartData(data);
+          const chartData = buildChartData(data, casesType);
           setData(chartData);
         });
     };
